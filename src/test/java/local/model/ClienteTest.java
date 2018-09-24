7efcd7017af23b303652e89f605597bb193cbd72
Angelo Luz <angelogl@gmail.com>
2018-09-24 10:48:02 -0300

    Corrige lista de TODO

--- src/test/java/local/model/ClienteTest.java
+++ src/test/java/local/model/ClienteTest.java
@@ -1,7 +1,7 @@
 package local.model;
 
 public class ClienteTest {
-    //TODO: O nome não pode ser nulo. Lança RuntimeException -  Nome não é um campo obrigatório
+    //TODO: O nome não pode ser nulo. Lança ClienteException -  Nome é um campo obrigatório
     //TODO: nome deve possuir entre 4 e 55 caracteres (inclusive). Lança ClienteException - O nome do cliente deve possuir entre 4 e 55 caracteres
     //TODO: O nome do cadastro deve possuir pelo menos 2 nomes (ex.: Angelo Luz)
     //TODO: O nome deverá ser salvo sem espaços no início e fim
--- src/test/java/local/model/FilmeTest.java
+++ src/test/java/local/model/FilmeTest.java
@@ -6,9 +6,9 @@ import org.junit.Test;
 import javax.validation.Valid;
 
 public class FilmeTest {
-    //TODO: Nome deve possuir entre 2 e 99 caracteres (inclusive)
-    //TODO: Estoque não pode ser negativo, e o máximo é 99 (inclusive)
-    //TODO: O preço de Locacao deverá ser um número positivo entre R$ 1,00 e R$ 9,99 (inclusive)
+    //TODO: Nome deve possuir entre 2 e 99 caracteres (inclusive) Lança FilmeException - O nome do filme deve possuir entre 2 e 99 caracteres
+    //TODO: Estoque não pode ser negativo, e o máximo é 99 (inclusive) Lança FilmeException - Valor de estoque inválido
+    //TODO: O preço de Locacao deverá ser um número positivo entre R$ 1,00 e R$ 9,99 (inclusive) Lança FilmeException - Valor locaçãa inválido
 
 
 
